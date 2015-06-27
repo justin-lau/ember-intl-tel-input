6d87785c32aef2fef8ab6c5a1a26bb2b6b16e13a
Justin Lau <justin@tclau.com>
2015-06-27 23:07:14 +0800

    Updated `intl-tel-input` version.

--- blueprints/ember-intl-tel-input/index.js
+++ blueprints/ember-intl-tel-input/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('intl-tel-input', '25b9f387bc616ca8f432defb35b918d45ab79432');
+    return this.addBowerPackageToProject('intl-tel-input', '~6.0.3');
   }
 };
